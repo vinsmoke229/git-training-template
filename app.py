4cd4cbf2e600fbe5e2b764a46b754a6b72dec222
vinsmoke229 <elleira072@gmail.com>
2026-05-28 15:26:19 +0000

    feat : ajout

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ def add(a, b):
 if __name__ == "__main__":
     print("--- Ma Super Toolbox ---")
     print(f"Résultat addition : {add(5, 5)}")
+def add(a, b):
+    return a + b
+
+if __name__ == "__madef add(a, b):
+    return a + b
+
+if __name__ == "__ma
